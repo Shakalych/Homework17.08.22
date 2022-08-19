ad973077a92b8377b854bd8943c703a7b2d2f403
Shakalych <madstabr@gmail.com>
2022-08-20 02:06:35 +0500

    Задача из семинара

---
+++ Exam7/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача из семинара!
+
+Console.WriteLine("Введите день недели (1-7)");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (x == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (x == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (x == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (x == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (x == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (x == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
