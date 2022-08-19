2b55630ff3163d68bb739c612b2b80f1387b00ab
Shakalych <madstabr@gmail.com>
2022-08-19 20:06:27 +0500

    Четверта задача ,но вопросик есть

---
+++ Exam4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int x = 1; x < num + 1; x++)       
+    if (x%2==0)
+       {
+        Console.WriteLine(x);
+       } 
+
+
+
+
